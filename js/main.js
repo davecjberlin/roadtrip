2d96003a784e79747ea04bc3242ae71936a496db
David Jenkins <37330327+davecjberlin@users.noreply.github.com>
2018-05-03 19:31:11 -0700

    First upload of index and a few other files
    Just the basics. Version 1.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Make it rain!
+console.log("helloworld")
+
+$( function() {
+    var handle = $( "#custom-handle" );
+    $( "#slider" ).slider({
+      create: function() {
+        handle.text( $( this ).slider( "value" ) );
+      },
+      slide: function( event, ui ) {
+        handle.text( ui.value );
+      }
+    });
+  } );
+
+
